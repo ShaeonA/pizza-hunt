130215e6f634fda42f6330f8ead1fe932d4a6467
Shaeon <Shaeonada@gmail.com>
2021-09-30 15:28:29 -0400

    updated routes

---
+++ routes/API/pizza-routes.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+  } = require('../../controllers/pizza-controller')
+
+  // /api/pizzas
+router
+.route('/')
+.get(getAllPizza)
+.post(createPizza);
+
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
